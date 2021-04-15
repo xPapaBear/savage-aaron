40f742bbb70b665997aa9c4c7d3efe82f8bf207f
mEnriquez2020 <mark.enriquez@debutify.com>
2021-04-15 14:09:18 +0800

    fix customers issue

--- resources/js/views/CustomerEntries.js
+++ resources/js/views/CustomerEntries.js
@@ -207,7 +207,7 @@ const CustomerEntries = ({ location }) => {
       fetchFinal = true
       setTimeout(async () => {
         if ( fetchFinal ) {
-          handleFetchFiltered();
+          // handleFetchFiltered();
         }
       }, 700)
     }
@@ -224,9 +224,11 @@ const CustomerEntries = ({ location }) => {
     <Badge status="info">{shopApi ? currencyFormat(shopApi, total_spent) : total_spent}</Badge>,
   ]
 
-  const rows = ! isEmpty(filteredCustomers) ? filteredCustomers?.data?.map(
-    (data) => rowsFormat(data)) : ! isEmpty(customers) ? customers?.data?.map(
-    (data) => rowsFormat(data)) : null;
+  // const rows = ! isEmpty(filteredCustomers) ? filteredCustomers?.data?.map(
+  //   (data) => rowsFormat(data)) : ! isEmpty(customers) ? customers?.data?.map(
+  //   (data) => rowsFormat(data)) : null;
+
+  const rows = ! isEmpty(customers) ? customers?.map((data) => rowsFormat(data)) : null;
 
   const pageMarkup = (
     <Page
