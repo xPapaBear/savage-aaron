22e1034fc1cd2cdcf65a215ffe59753bef8d4e08
Mark <mark.enriquez@debutify.com>
2021-05-01 01:14:14 +0800

    heroku

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\Multiplier::factory(1)->create();
-        CustomerEntriesSeeder::class;
+        // CustomerEntriesSeeder::class;
     }
 }
